b0710ea0af2605bd1a343ea7f7cddd58fdb5b825
leihtg <leihtg@163.com>
2018-01-09 17:43:44 +0800

    server handler

--- core/src/main/java/com/youme/contant/Contant.java
+++ core/src/main/java/com/youme/contant/Contant.java
@@ -5,8 +5,10 @@ package com.youme.contant;
  */
 
 public class Contant {
-    //广播端口
-    public static final int BROCAST_PORT = 12345;
-    //正常服务端口号
-    public static final int PORT = 12567;
+    // 广播端口,客户端/服务端各自监听自己本地的端口号
+    public static final int BROCAST_PORT = 7777;
+    // 手机端端口号
+    public static final int CLIENT_PORT = 7788;
+    // 主机地址信息
+    public static final byte HOST_MSG = 0x01;
 }
--- core/src/main/java/com/youme/server/TCPSingleton.java
+++ core/src/main/java/com/youme/server/TCPSingleton.java
@@ -1,5 +1,8 @@
 package com.youme.server;
 
+import android.os.Handler;
+import android.os.Message;
+
 import com.youme.contant.Contant;
 
 import java.io.IOException;
@@ -7,24 +10,66 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
-import java.net.UnknownHostException;
+import java.util.Arrays;
 
 /**
  * create at 2018年1月8日17:39:49
  */
 public class TCPSingleton {
-    public void sendBrocast() throws IOException {
-        InetAddress.getLocalHost();
+
+    private static Handler connHandler;
+
+    private TCPSingleton() {
+        server.start();
+    }
+
+    public void brocastLocalHost() throws IOException {
         InetAddress address = InetAddress.getByName("255.255.255.255");
-        byte[] msg = "".getBytes();
-        DatagramPacket packet = new DatagramPacket(msg, msg.length, address, Contant.BROCAST_PORT);
+        byte[] hostMsg = new byte[]{Contant.HOST_MSG};
+        DatagramPacket packet = new DatagramPacket(hostMsg, hostMsg.length, address, Contant.BROCAST_PORT);
 
         DatagramSocket ds = new DatagramSocket();
         ds.setBroadcast(true);
         ds.send(packet);
     }
 
-    public static void main(String[] args) throws UnknownHostException {
-        System.out.println(InetAddress.getLocalHost());
+    Thread server = new Thread() {
+        @Override
+        public void run() {
+            try {
+                DatagramSocket ds = new DatagramSocket(Contant.CLIENT_PORT);
+                ds.setBroadcast(true);
+                while (true) {
+                    byte[] buf = new byte[2048];
+                    DatagramPacket packet = new DatagramPacket(buf, buf.length);
+                    ds.receive(packet);
+                    // 接收的数据
+                    buf = Arrays.copyOf(packet.getData(), packet.getLength());
+                    if (null != buf) {
+                        switch (buf[0]) {
+                            case Contant.HOST_MSG:
+                                InetAddress hostAddr = packet.getAddress();
+                                System.out.println("主机地址:" + hostAddr);
+                                if (null != connHandler) {
+                                    Message msg = new Message();
+                                    msg.what = Contant.HOST_MSG;
+                                    msg.obj = hostAddr;
+                                    connHandler.sendMessage(msg);
+                                }
+                        }
+                    }
+                }
+            } catch (SocketException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    };
+
+
+    public static void main(String[] args) throws IOException {
+        TCPSingleton t = new TCPSingleton();
+        t.brocastLocalHost();
     }
 }
