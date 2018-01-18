56c95dd9b5871f6f9a294e42948d3c76112b4bb3
leihtg <leihtg@163.com>
2018-01-18 17:17:16 +0800

    about

--- app/src/main/java/com/youme/activity/CustomInfoActivity.java
+++ app/src/main/java/com/youme/activity/CustomInfoActivity.java
@@ -2,17 +2,31 @@ package com.youme.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.youme.R;
 
 /**
  * Create at 2018年1月6日22:34:47
  */
-public class CustomInfoActivity extends AppCompatActivity {
+public class CustomInfoActivity extends AppCompatActivity implements View.OnClickListener {
+    ImageView back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_info);
+        back = (ImageView) findViewById(R.id.custInfo_back);
+        back.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.custInfo_back:
+                super.onBackPressed();
+                break;
+        }
     }
 }
